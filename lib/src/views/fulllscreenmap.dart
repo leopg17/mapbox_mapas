0d15cec90c5f4e28a68239e10940bb799155c109
Leonardo <leopg17@gmail.com>
2020-11-20 21:55:51 -0600

    Inicialización del proyecto

---
+++ lib/src/views/fulllscreenmap.dart
@@ -0,0 +1,15 @@
+//Creamos un widget que sea el encargado de mostrar el mapa en pantalla completa
+import 'package:flutter/material.dart';
+
+class FullScreenMap extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text('Hola Mundo'),
+        ),
+      ),
+    );
+  }
+}
