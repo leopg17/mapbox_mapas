c79d7b4094b5b4abf55d6291f99e931ec1648105
Leonardo <leopg17@gmail.com>
2020-11-21 21:20:04 -0600

    Mostrar el Mapa usando MapboxGL

--- lib/src/views/fulllscreenmap.dart
+++ lib/src/views/fulllscreenmap.dart
@@ -1,15 +1,32 @@
 //Creamos un widget que sea el encargado de mostrar el mapa en pantalla completa
 import 'package:flutter/material.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 
-class FullScreenMap extends StatelessWidget {
+class FullScreenMap extends StatefulWidget {
+
+  @override
+  _FullScreenMapState createState() => _FullScreenMapState();
+}
+
+class _FullScreenMapState extends State<FullScreenMap> {
+  MapboxMapController mapController;
+
+  void _onMapCreated(MapboxMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          child: Text('Hola Mundo'),
-        ),
-      ),
+      body: MapboxMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition:
+          const CameraPosition
+          (
+            target: LatLng(40.76815605079246, -73.97439239749016),
+            zoom: 14
+          ),
+        )
     );
   }
 }
